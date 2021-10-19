d919018f6bf11f5bc95b961d84bcc9a3c463edbf
David Lancheros <davidlancherosg@gmail.com>
2021-10-19 10:38:46 +0200

    finish

--- routes/artist.route.js
+++ routes/artist.route.js
@@ -44,13 +44,23 @@ router.post('/', async function (req, res, next) {
       }
   })
 
-  router.post("/update/:id([a-z0-9]{24})", async (req, res, next) => {
-   
-    artistModel.findByIdAndUpdate(req.params.id,{
-        req.body
-    });
+  router.post("/update/:id([a-z0-9]{24})", async function (req, res, next) {
+    if (req.body.isBand === 'on') {req.body.isBand = true}
+    else {req.body.isBand = false};
+    try {
+      await artistModel.findByIdAndUpdate(req.params.id, req.body);
+      res.redirect("/dashboard/artist");
+    } catch (err) {
+      next(err);
+    }
   });
 
+
+router.get("/delete/:id([a-z0-9]{24})", (req, res, next) => {
+  artistModel.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/dashboard/artist"))
+    .catch(next);
+});
 
 
 
